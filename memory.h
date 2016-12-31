7db44857f8df50d027e25dcf8f2150ec12c94ee3
Michael Hofer <hoferm90@gmail.com>
2016-12-31 23:22:03 +0100

    idk

--- memory.h
+++ memory.h
@@ -1,5 +1,14 @@
-unsigned char memory[65536];
+enum {
+	ROM,
+	MBC1,
+	MBC1_RAM,
+	MBC1_RAM_BAT
+} mode;
 
-void read_rom(void);
+void read_rom(const unsigned char *buffer);
+
+void write_memory(unsigned short addr, unsigned char value);
+
+unsigned char read_memory(unsigned short addr);
 
 int init_memory(void);
