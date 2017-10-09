4d36f0f116972528193cebe08204c01bb7aedf62
Michael Hofer <hoferm90@gmail.com>
2017-10-09 19:27:33 +0200

    Implement drawing of tiles

--- video.c
+++ video.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 
-#include <SDL2/SDL.h>
-
 #include "gameboy.h"
 
+#include "cpu.h"
 #include "interrupt.h"
 #include "error.h"
 #include "memory.h"
@@ -20,23 +19,31 @@ struct sprite {
 	u8 flags;
 };
 
-struct point {
-	SDL_Point points[64];
-	int color;
-};
-
 static u8 *vram;
 static u16 max_vram_range = 0x97FF;
 
 static int palette[4] = { 0xFFFFFF, 0xB2B2B2, 0x666666, 0x0 };
 
 static u8 lcdc_register;
+static int display_enable;
 
-static void tile_data(u8 *tile, u8 tile_nr)
+static int ly_count;
+
+static void update_palette(int *palette)
 {
-	int i, j;
+	u8 bgp_data = read_memory(0xFF47);
+
+	palette[0] = bgp_data & 0x3;
+	palette[1] = (bgp_data >> 2) & 0x3;
+	palette[2] = (bgp_data >> 4) & 0x3;
+	palette[3] = (bgp_data >> 6) & 0x3;
+}
+
+/* partial visible tiles? */
+static void tile_data(u8 *tile, u8 tile_nr, int size)
+{
+	int j;
 	int start = tile_nr * 16;
-	int end = start + 16;
 	u8 color;
 	u8 lsb, msb;
 	int offset = 0;
@@ -48,88 +55,88 @@ static void tile_data(u8 *tile, u8 tile_nr)
 		offset = 0x800;
 	}
 
-	for (i = start; i < end; i += 2) {
-		lsb = vram[i];
-		msb = vram[i + 1];
+	lsb = vram[start];
+	msb = vram[start + 1];
 
-		for (j = 7; j >= 0; --j) {
-			color = ((lsb >> j) & 0x1) + (((msb >> j) & 0x1) << 1);
+	for (j = size; j >= 0; --j) {
+		color = ((lsb >> j) & 0x1) + (((msb >> j) & 0x1) << 1);
 
-			if (j == 0)
-				*(tile + offset + ((i / 2) * 8) + 7) = color;
-			else
-				*(tile + offset + ((i / 2) * 8) + (j % 7)) = color;
-		}
+		if (j == 0)
+			*(tile + offset + 7) = color;
+		else
+			*(tile + offset + (j % 7)) = color;
 	}
 }
 
-static u8 get_scy(void)
+/* Draw tiles per line */
+static void draw_tiles(u8 *line, u8 ly)
 {
-	return read_memory(0xFF42);
+	u8 scy = read_memory(0xFF42) + ly;
+	u8 scx = read_memory(0xFF43);
+	int i, j = 0;
+	int k = 0;
+	u8 tile_nr;
+	int offset = scy * WIDTH;
+
+	for (i = scx + 1, tile_nr = vram[scx + offset]; i < WIDTH; i++) {
+		if (tile_nr == vram[i + offset]) {
+			k++;
+			tile_nr = vram[i + offset];
+			continue;
+		}
+		tile_data(line + j, tile_nr, k);
+
+		j++;
+	}
 }
 
-static u8 get_scx(void)
+static void draw_sprites(void)
 {
-	return read_memory(0xFF43);
+	/* TODO */
 }
 
-int screen_enabled(void)
-{
-	return BIT_7(lcdc_register);
-}
-
-static void draw_blank(void)
-{
-
-}
-
-static void draw_scanline(SDL_Point *pt)
+/* TODO: Use enum for return */
+int draw_scanline(void)
 {
 	int bg_palette[4] = { 0, 1, 2, 3 };
-	u8 scanline = read_memory(0xFF44);
-	u8 bgp_data = read_memory(0xFF47);
-	u8 scy = get_scy();
-	u8 scx = get_scx();
+	u8 stat = read_memory(0xFF41);
+	u8 ly = read_memory(0xFF44);
+	u8 lyc = read_memory(0xFF45);
+	int inc = cpu_cycle() - old_cpu_cycle();
+	u8 line[WIDTH];
 
-	bg_palette[0] = bgp_data & 0x3;
-	bg_palette[1] = (bgp_data >> 2) & 0x3;
-	bg_palette[2] = (bgp_data >> 4) & 0x3;
-	bg_palette[3] = (bgp_data >> 6) & 0x3;
+	if (!display_enable)
+		return -1;
 
-	if (!screen_enabled()) {
-		draw_blank();
-	} else {
-	}
+	ly_count += inc;
+
+	if (ly_count < 252)
+		return -1;
+
+	if (ly >= 144)
+		request_interrupt(INT_VBLANK);
+
+	if (ly == lyc)
+		write_memory(0xFF41, set_bit(stat, 2));
+
+	draw_tiles(line, ly);
+	draw_sprites();
+
+	write_memory(0xFF44, ly + 1);
+
+	return 0;
 }
 
-struct point line(SDL_Point *pt)
+void update_registers(void)
 {
-	struct point p;
-	p.color = 0;
-	draw_scanline(pt);
-
-	return p;
-}
-
-static void display_tiles(void)
-{
-	u8 scy = get_scy();
-	u8 scx = get_scx();
-	int offsetx = scx / 8;
-	int offsety = scy / 8;
-	int i, j;
-	u8 tile[64];
-
-	for (i = offsetx; i < WIDTH; ++i) {
-		for (j = offsety; j < HEIGHT; ++j) {
-			tile_data(tile, vram[i+j]);
-		}
-	}
-}
-
-static void update_registers(void)
-{
+	int tmp;
 	lcdc_register = read_memory(0xFF40);
+	tmp = get_bit(lcdc_register, 7);
+
+	if (display_enable == 0 && tmp == 1)
+		write_memory(0xFF44, 0);
+
+	display_enable = tmp;
 }
 
 void init_vram(void)
