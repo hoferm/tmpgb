462c54d91ff8e9898b92136b06d8c330074d9684
Michael Hofer <hoferm90@gmail.com>
2017-10-12 06:52:26 +0200

    Improve debugger
    Add new commands for memory inspection.
    Improve disassemble to skip next 1 or 2 bytes if these are parameters to
    an opcode.

--- debug.c
+++ debug.c
@@ -11,17 +11,32 @@ static struct cpu_info cpu;
 
 static void cursor(void)
 {
-	printf("    at 0x%.4X >> ", *cpu.PC);
+	printf("at 0x%.4X >> ", *cpu.PC);
 }
 
 static void disassemble(int n)
 {
-	int i;
+	int i, j;
 	u8 opcode;
+	u16 op_param;
 
-	for (i = 0; i < n; i++) {
+	for (i = 0, j = 0; j < n; j++, i++) {
 		opcode = read_memory((*cpu.PC) + i);
-		printf("\t0x%.4X (%s)\n", (*cpu.PC) + i, op_names[opcode]);
+
+		printf("\t");
+		if (strstr(op_names[opcode], "%.4X") != NULL) {
+			op_param = read_memory(cpu.PC[0] + i + 1) +
+				(read_memory(cpu.PC[0] + i + 2) << 8);
+			printf(op_names[opcode], op_param);
+			i += 2;
+		} else if (strstr(op_names[opcode], "%.2X") != NULL) {
+			printf(op_names[opcode], read_memory((*cpu.PC) + i + 1));
+			i++;
+		} else {
+			printf(op_names[opcode]);
+		}
+
+		printf("\n");
 	}
 }
 
@@ -31,30 +46,48 @@ static void step(void)
 	fetch_opcode();
 }
 
-void debug(void)
+static void view_mem(u16 start, u16 end)
 {
-	char buf[16];
-	char cmd[16];
-	int param;
-	int n;
+	int i;
+	int j = 0;
 
-	cursor();
-	if (fgets(buf, 16, stdin) == NULL)
-		die("debug: could not read from stdin");
-
-	n = sscanf(buf, "%s", cmd);
-
-	if (n < 1) {
+	if (start > end) {
+		i = end;
+		end = start;
+		start = i;
 	}
 
-	if (!strcmp(cmd, "s")) {
+	for (i = start; i <= end; i++) {
+		if (j % 4 == 0)
+			printf("\n");
+		printf("\t0x%.4X: %.2X", i, read_memory(i));
+		j++;
+	}
+	printf("\n");
+}
+
+void debug(void)
+{
+	char cmd[16];
+	char tmp[16];
+	unsigned param;
+	/* unsigned param2; */
+
+	cursor();
+	if (fgets(cmd, 16, stdin) == NULL)
+		die("debug: could not read from stdin");
+
+	if (!strcmp(cmd, "s\n")) {
 		step();
-	} else if (sscanf(buf, "%s %d\n", cmd, &param) >= 1) {
-		if (!strcmp(cmd, "d")) {
-			disassemble(param);
-		}
+	} else if (sscanf(cmd, "d %ud\n", &param) >= 1) {
+		disassemble(param);
+	} else if (sscanf(cmd, "m 0x%X\n", &param) >= 1) {
+		view_mem(param, param);
+	} else if (sscanf(cmd, "m %s\n", tmp) >= 1) {
+		if (!strcmp(tmp, "io"))
+			view_mem(0xFF00, 0xFF7F);
 	} else {
-		printf("Unknown command\n");
+		printf("\n\tUnknown command\n");
 		return;
 	}
 }
